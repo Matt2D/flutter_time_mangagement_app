46c6b41fbe470f7fa28869f9b976f087d7b5c87b
Matt2D <mattsimoni2d@gmail.com>
2023-08-09 10:48:27 -0400

    Original Commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/matthewsimoni/Desktop/flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/matthewsimoni/Desktop/flutter/flutterProject"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_ENGINE=/Users/matthewsimoni/Desktop/flutter/engine/src"
+export "LOCAL_ENGINE=ios_debug_sim_unopt"
+export "ARCHS=x86_64"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=true"
+export "PACKAGE_CONFIG=/Users/matthewsimoni/Desktop/flutter/flutterProject/.dart_tool/package_config.json"
